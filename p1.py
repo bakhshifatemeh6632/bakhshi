e490803e9334a74f5f8128c4adf56292e78b089a
bakhshifatemeh6632 <47629963+bakhshifatemeh6632@users.noreply.github.com>
2022-06-03 03:08:08 +0430

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,8 @@
+pi=22/7
+heghit=float(input("Enter Number for Heghit:"))
+radian=float(input("Enter Number fot Radian:"))
+Voul=pi*radian*radian*heghit
+masahat=((2*pi*radian)*heghit)+(pi*(radian**2)*2)
+print("vol:",' ', Voul)
+print("masahat:",' ',masahat )
+
